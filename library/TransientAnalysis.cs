b890cc6a24b6a56e89a161a708248b764966fe49
Gen Li <597974733.qq@gmail.com>
2019-11-23 22:02:28 +0800

    minor mods

--- library/TransientAnalysis.cs
+++ library/TransientAnalysis.cs
@@ -5,14 +5,7 @@ using MathNet.Numerics.LinearAlgebra;
 
 namespace library {
     public class TransientAnalysis {
-
-        public static double stoptime;
-        public static double timestep;
-
-        // TODO: Setters for the time parameters
-
-
-        public static Circuit GenCompanion(in Circuit ckt, in double currentTime, in TransientAnalysisResult result) {
+        public static Circuit GenCompanion(in Circuit ckt, in double currentTime, in TransientAnalysisData data) {
             /* Generate a companion of the circuit for the transient analysis. */
             var companion = new Circuit($"__Companion_{ckt.name}");
             foreach (var device in ckt.devices) {
@@ -21,16 +14,16 @@ namespace library {
                         // Here we use p and n to represent the positive/negative pin of the imaginary ISource Ieq.
                         var (p, n) = (c.pins[0], c.pins[1]);
                         // Get the previously calculated voltage between the two pins.
-                        var vPrev = result.history.Last()[p.ID] - result.history.Last()[n.ID];
+                        var vPrev = data.result.Last()[p.ID] - data.result.Last()[n.ID];
                         companion.AddComponent(new Resistor(
                             $"__CompanionRes_{c.name}",
-                            timestep / c.capacitance,
+                            data.timestep / c.capacitance,
                             p,
                             n
                         ));
                         companion.AddComponent(new PrimISource(
                             $"__CompanionISource_{c.name}",
-                            vPrev * timestep / c.capacitance,
+                            vPrev * data.timestep / c.capacitance,
                             p,
                             n
                         ));
@@ -47,31 +40,38 @@ namespace library {
         }
 
 
-        public void Analyze(in Circuit ckt, ref TransientAnalysisResult result) {
-            /* Perform the transient analysis and store the result in the given TransientAnalysisResult variable. */
+        public void Analyze(in Circuit ckt, ref TransientAnalysisData data) {
+            /* Perform the transient analysis and store the result in the given TransientAnalysisData variable. */
             // Start the simulation with a DC analysis.
-            result.Add(DCAnalysis.SolveX(ckt));
-            for (double currentTime = timestep; currentTime < stoptime; currentTime += timestep) {
-                var companion = TransientAnalysis.GenCompanion(ckt, currentTime, result);
-                result.Add(DCAnalysis.SolveX(companion));
+            data.Add(DCAnalysis.SolveX(ckt));
+            for (double currentTime = data.timestep; currentTime < data.stoptime; currentTime += data.timestep) {
+                var companion = TransientAnalysis.GenCompanion(ckt, currentTime, data);
+                data.Add(DCAnalysis.SolveX(companion));
             }
         }
     }
 
-    public class TransientAnalysisResult {
-        public List<Vector<double>> history;
+    public class TransientAnalysisData {
+        /* A class containing the transient analysis parameters and result. */
+        public List<Vector<double>> result;
+        public double stoptime;
+        public double timestep;
 
-        public TransientAnalysisResult(Circuit ckt) {
+        public TransientAnalysisData(Circuit ckt, double timestep, double stoptime) {
             var builder = Vector<double>.Build;
             var n = ckt.N;
             var m = ckt.M;
             var size = n + m;
 
-            history = new List<Vector<double>>();
+            this.result = new List<Vector<double>>();
+
+            this.timestep = timestep;
+            this.stoptime = stoptime;
         }
 
-        public void Add(Vector<double> record) {
-            this.history.Add(record);
+        public void Add(Vector<double> item) {
+            // Add a result vector to the history.
+            this.result.Add(item);
         }
     }
 }
